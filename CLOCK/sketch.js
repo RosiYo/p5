86313956b83cf00761da008d469123f62d572b33
RosiYo <RosiYo@users.noreply.github.com>
2018-02-12 00:34:08 +0100

    CLOCK_FINISHED

--- CLOCK/sketch.js
+++ CLOCK/sketch.js
@@ -1,18 +1,24 @@
-
+// Inspired by a chalenge
+  // I did this Code because I was very busy, and I wasn't able to do something difficult ;)
 function setup() {
   createCanvas(600,600)
-  angleMode(DEGREES)
+  angleMode(DEGREES) //I prefer dgrees to radians, so I changed it
 }
 
 function draw() {
   background(51)
+  //This are all the variables I used
   var minDg = minute()*6;
   var sgDg = second()*6;
   var hrDg = hour()*15;
+  var hr = hour()
+  var min = minute()
+  var sg = second()
   var yr = year()
   var dy = day()
   var mth = month()
-
+  var mll = millis()
+//This are the circles of hours, minutes and seconds
   push()
   translate(width/2,height/2)
   rotate(-90)
@@ -25,49 +31,62 @@ function draw() {
   stroke(51,51,153)
   arc(0,0,300,300,0,hrDg)
   pop()
-
+//The lines in the seconds' circle
   push()
   strokeWeight(4)
   translate(width/2,height/2)
   rotate(-90)
   for (var i=0;i<60;i++){
-    if (sgDg/6 >= i)  {
+    if (sg >= i)  {
       stroke(102,153,255)
       line(190,0,200,0)
       rotate(6)
     }
   }
   pop()
-
+//The lines in the minutes' circle
   push()
   strokeWeight(4)
   translate(width/2,height/2)
   rotate(-90)
   for (var i=0;i<60;i++){
-    if (minDg/6 >= i)  {
+    if (min >= i)  {
       stroke(51,102,204)
       line(165,0,175,0)
       rotate(6)
     }
   }
   pop()
-
+//The lines in the hours' circle
   push()
   strokeWeight(4)
   translate(width/2,height/2)
   rotate(-90)
   for (var i=0;i<24;i++){
-    if (hrDg/15 >= i)  {
+    if (hr >= i)  {
       stroke(51,51,153)
       line(140,0,150,0)
       rotate(15)
     }
   }
   pop()
-
+//White rectangle
+  rectMode(CENTER)
+  fill(240)
+  noStroke()
+  rect(300,320,130,40)
+//Dates and numbers in the screen
+  fill(255)
+  textAlign(LEFT)
+  textSize(18)
+  text("MILISECONDS:"+floor(mll),420,20)
+  textAlign(CENTER)
+  textSize(30)
+  fill(30)
+  text(hr+":"+min+":"+sg,300,330)
+  textSize(40)
   textFont("Century Gothic")
   fill(100,250,200)
-  textSize(40)
-  textAlign(CENTER)
-  text(dy+"/"+mth+"/"+yr,300,300)
+  text(dy+"/"+mth+"/"+yr,300,290)
 }
+  //By Rosi
